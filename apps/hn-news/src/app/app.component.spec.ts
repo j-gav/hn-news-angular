62e0c95f21cb1174fc5c0776a27c60bd43b476de
John Gavin <john.gavin@genesys.com>
2025-03-03 11:34:56 +0000

    tests: fix bust test

--- apps/hn-news/src/app/app.component.spec.ts
+++ apps/hn-news/src/app/app.component.spec.ts
@@ -13,9 +13,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome hn-news'
-    );
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hacker news application');
   });
 
   it(`should have as title 'hn-news'`, () => {
